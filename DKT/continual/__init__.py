c2beb9550cc134d6941f5cb2209cff65c6ba4c5b
misaka-mio <67531633+misaka-mio@users.noreply.github.com>
2023-06-19 23:27:39 +0800

    Delete DKT directory

--- DKT/continual/__init__.py
+++
@@ -1,6 +0,0 @@
-from continual import rehearsal
-from continual import classifier
-from continual import utils
-from continual import scaler
-from continual import factory
-from continual import samplers
